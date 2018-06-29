aae4e9cab9fe91ee4d640183486397fc148c1949
sekcheong <scheong@gmail.com>
2018-06-29 14:45:18 -0500

    add some comment

--- zlaser_cs/Program.cs
+++ zlaser_cs/Program.cs
@@ -23,20 +23,28 @@ namespace zlaser_cs
 
 		static void Main(string[] args)
 		{
-			int comm = 0;
+			//we use handle to keep track of objects in DLL, hObj can simply be an index 
+			//to an object entry in an array in C that stores the C++ objects
+		
+			int hObj = 0;
 
 			//shows how to pass int by reference back to C# from dll
-			zl_device_open("1234", ref comm);
+			zl_device_open("1234", ref hObj);
+
+			Console.WriteLine("hObj=" + hObj);
+
 			StringBuilder sb = new StringBuilder(100);
 			int len = 0;
 
 			//shows how to pass string back to C# from dll
-			zl_device_read(comm, sb, sb.Capacity, ref len);			
+			zl_device_read(hObj, sb, sb.Capacity, ref len);			
 			Console.WriteLine(sb.ToString());
 
 			//shows how to pass C# array to C dll
 			byte[] imageData = new byte[100];
 
+			//to access C# array directly from C the array object must be pinned down 
+			//in memory or it might get destroyed by the C# garbage collector
 			GCHandle handle;
 			handle = GCHandle.Alloc(imageData, GCHandleType.Pinned);
 			IntPtr outBuffer = handle.AddrOfPinnedObject();
@@ -48,7 +56,8 @@ namespace zlaser_cs
 				Console.Write(imageData[i] + " ");
 			}
 
-			zl_device_close(comm);
+			//this should destroy the object associated to hObj
+			zl_device_close(hObj);
 		}
 	}
 }
